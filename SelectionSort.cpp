725d21da2ea294c126e9118bc8a4012bd01fc1fd
rlama7 <royalray7@gmail.com>
2018-08-06 18:44:22 -0700

    changed the algorithm steps to be more concise.

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -5,12 +5,16 @@
  * @date		07/27/2018
  * 
  * @Algorithm
- * Inputs an array to be sorted
- * A) for i = 0 to Array length -1, set minimum to i
- * B) for j = i+1 to Array length :
- * 		a) if A[j] < A[minimum]
- * 				then set minimum to j
- * C) Swap A[i] with A[minimum]
+ * 
+ * Smallest --> Final Position
+ * Steps:
+ * 1) Begin with first element in the array as the minimum value.
+ * 2) Compare the minimum value with the next element in the array
+ * 3) If the minimum value > the next element swap the value
+ * 4) else move to the next element to compare.
+ * 5) At the end of each pass swap the minimum value to the correct index position in the array
+ * 6) Repeat the process until values are in their proper position
+ * 
  * Outputs a sorted array in ascending order
  */
  
@@ -19,18 +23,18 @@
 using namespace std;
 
 // Function Prototypes
-void SelectionSort(int list[], int size);		
+void SelectionSort(int Array[], int array_size);		
 
 
 // Begin Main
 int main() {
-	int list[] = {2,7,4,1,3,0,9};
+	int Array[] = {2,7,4,1,3,0,9};
 		
-	SelectionSort(list, 7);
+	SelectionSort(Array, 7);
 	
 	cout << "[";
 	for (int i=0; i<7; i++) {
-		cout << list[i] << " ";
+		cout << Array[i] << " ";
 	}
 	cout << "]";
 	
@@ -41,20 +45,20 @@ int main() {
 
 // Function SelectionSort()
 
-void SelectionSort(int sort_list[], int size) {
+void SelectionSort(int Array[], int array_size) {
 	
-	for (int i=0; i < size-1; i++) {		// we need to n-2 passes
-		int iMin = i;					// ith position: elements from i till n-1 are candidates
+	for (int i=0; i < array_size; i++) {
+		int iMin = i;								// consider 0th element as the minimum value to begin with
 		
-		for (int j = i+1; j < size; j++) {
+		for (int j = i+1; j < array_size; j++) {	// we start comparing minimum value with the next value 
 			
-			if (sort_list[j] < sort_list[iMin]) {		// if we find a smaller value
-				iMin = j;				// update the index of minimum element
+			if (Array[j] < Array[iMin]) {		// if we find a smaller value
+				iMin = j;						// update the index of minimum element
 			}
 		}
-		
-		int temp = sort_list[i];
-		sort_list[i] = sort_list[iMin];
-		sort_list[iMin] = temp;				// swap
+		// after we exhaust comparison we swap the minimum value 
+		// at the correct index position in the array
+		// Using swap function
+		swap(Array[i], Array[iMin]);
 	}
 } // end SelectionSort()
